343440994a593e5b527b657f519278d58a2abaab
Nikita <minzarnikita@gmail.com>
2022-09-30 12:13:41 +0300

    i fixed my code

--- js/script.js
+++ js/script.js
@@ -154,12 +154,13 @@ window.addEventListener('DOMContentLoaded', () =>{
             // Использовать классы для карточек
 
             class MenuCard  {
-                constructor (src, alt, title, descr, price, perentSelector) {
+                constructor (src, alt, title, descr, price, perentSelector, ...classes) {
                     this.src = src;
                     this.alt = alt;
                     this.title = title;
                     this.descr = descr;
                     this.price = price;
+                    this.classes = classes;
                     this.perentSelector = document.querySelector(perentSelector);
                     this.transfer = 42;
                     this.cheangeToUAH();
@@ -170,18 +171,22 @@ window.addEventListener('DOMContentLoaded', () =>{
 
                 render() {
                     const element = document.createElement('div');
+
+                    if (this.classes.length === 0) {
+                        this.element ='menu__item';
+                        element.classList.add(this.element);
+                    }else{
+                        this.classes.forEach(className => element.classList.add(className));
+                    } 
+                    
                     element.innerHTML = `
-                    <div class="container">
-                <div class="menu__item">
                     <img src=${this.src} alt=${this.alt}>
                     <h3 class="menu__item-subtitle">${this.title}</h3>
-                    <div class="menu__item-descr">${this.descr}></div>
+                    <div class="menu__item-descr">${this.descr}</div>
                     <div class="menu__item-price">
-                    <div class="menu__item-divider"></div>
                         <div class="menu__item-cost">Цена:</div>
-                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                        <div class="menu__item-total"><span>${this.price}</span>грн/день</div>
                     </div>
-                </div>
                     `;
                     this.perentSelector.append(element);
                 }
