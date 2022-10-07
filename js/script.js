81139f48da62ffed4d23638d65528b3820554256
Nikita <minzarnikita@gmail.com>
2022-10-07 13:05:33 +0300

    added fetch api

--- js/script.js
+++ js/script.js
@@ -245,29 +245,29 @@ window.addEventListener('DOMContentLoaded', () =>{
 
                     form.insertAdjacentElement('afterend', statusMessage);
 
-                    const request = new XMLHttpRequest();
-                    request.open('POST', 'server.php');
-                    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
                     const formData = new FormData(form);
 
                     const object = {};
                     formData.forEach(function(value, key){
                         object[key] = value;
                     });
-
-                    const json = JSON.stringify(object);
-
-                    request.send(json);
-
-                    request.addEventListener('load', () => {
-                        if (request.status === 200){
-                            console.log(request.response);
+                    fetch('server.php', {
+                        method: 'POST',
+                        headers: {
+                            'Content-type': 'application/json'
+                        },
+                        body: JSON.stringify(object)
+                    })
+                    .then(data => data.text())
+                    .then(data => {
+                            console.log(data);
                             showThanksModal(message.success);
                             statusMessage.remove();
                             form.reset();
-                        } else {
-                            showThanksModal(message.failure);
-                        }
+                    }).catch(() => {
+                        showThanksModal(message.failure);
+                    }).finally(() => {
+                        form.reset();
                     });
                 });
             }
